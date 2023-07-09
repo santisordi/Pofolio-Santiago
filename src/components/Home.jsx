ccb992fc6ff2864f9a70820066c176fad23ea9d1
Santiago Sordi <santiagosordi@gmail.com>
2023-07-09 09:53:48 -0300

    se inicializa porfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.jpg"
+
+export const Home = () => {
+    return (
+    <div>
+        <img src={HeroImage} alt="" />
+    </div>
+    )
+}
