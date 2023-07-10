e70b31e198bf3c4039d3c03d9fc976affc508b71
Santiago <santiagosordi@gmail.com>
2023-07-10 16:22:04 -0300

    se agrega array a componente portfolio y estilos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import reactPorfolio from "../assets/portfolio/PoryectoReactJS-Porfolio.png";
+import webLucrecia from "../assets/portfolio/ProyectoWeb-La Lucrecia.png";
+import reactRegionales from "../assets/portfolio/ProyectoReactJS-Regionales.png";
+import jsCotizador from "../assets/portfolio/ProyectoJS-CotizadorSeguros.png";
 
 const Portfolio = () => {
+
+  const portfolios = [
+    {
+      id: 1,
+      src: reactPorfolio
+    },
+    {
+      id: 2,
+      src: webLucrecia
+    },
+    {
+      id: 3,
+      src: reactRegionales
+    },
+    {
+      id: 4,
+      src: jsCotizador
+    }
+  ]
+
+
   return (
-    <div>Portfolio</div>
+    <div name="Porfolio" className='bg-gradient-to-b fromblack to bg-gray-800 w-full text-white md:h-screen'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+          <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Porfolio</p>
+            <p className='py-6'>Mira alguno de mis trabajos</p>
+          </div>
+
+          {
+            portfolios.map(({id, src}) => (
+
+           <div key={id} className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+            <div className='shadow-md shadow-gray-600 rounded-lg'>
+              <img src={src} alt='imagen proyecto react porfolio' className='rounded-md duration-200 hover:scale-105'/>
+              <div className='flex justify-center items-center'>
+                  <button className="w-1/2 px-6 m-4 duration-200 hover:scale-105">Demo</button>
+                  <button className="w-1/2 px-6 m-4 duration-200 hover:scale-105">Code</button>
+              </div>
+            </div>
+            </div>
+            ))
+          }
+        </div>
+    </div>
   )
 }
 
